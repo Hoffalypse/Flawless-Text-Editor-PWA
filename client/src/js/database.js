d85d6ffb12d158529bc72b22dd88dcc417f9166b
Bryan Wienhoff <106857564+Hoffalypse@users.noreply.github.com>
2022-11-01 22:16:15 -0600

    FIXED THE BUGSgit add -A

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -31,9 +31,9 @@ export const getDb = async () => {
     const jateDb = await openDB("jate", 1);
     const tx = jateDb.transaction("jate", "readonly");
     const store = tx.objectStore("jate");
-    const request = store.getAll();
+    const request = store.get(1);
     const result = await request;
-    return result;
+    return result.content;
 
   
   
--- client/webpack.config.js
+++ client/webpack.config.js
@@ -2,10 +2,9 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 
-// TODO: Add and configure workbox plugins for a service worker and manifest file.
-// TODO: Add CSS loaders and babel to webpack.
 
 module.exports = () => {
   
@@ -23,6 +22,7 @@ module.exports = () => {
     plugins: [
       new HtmlWebpackPlugin({
         template: './index.html',
+        favicon: "./favicon.ico"
        
       }),
     //added service worker and manfest file 
@@ -46,9 +46,11 @@ module.exports = () => {
         },
       ],
     }),
+    new MiniCssExtractPlugin(),
+
       new InjectManifest({
         swSrc: './src-sw.js',
-        swDest: 'created-sw.js',
+        // swDest: 'src-sw.js',
       }), 
     
     ],
@@ -57,12 +59,12 @@ module.exports = () => {
       rules: [
         {
           test: /\.css$/i,
-          use: ['style-loader', 'css-loader'],
+          use: [MiniCssExtractPlugin.loader, 'css-loader'],
         },
-        // {
-        //   test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        //   type: 'asset/resource',
-        // },
+        {
+          test: /\.(png|svg|jpg|jpeg|gif|ico)$/i,
+          type: 'asset/resource',
+        },
         {
           test: /\.m?js$/,
           exclude: /node_modules/,
